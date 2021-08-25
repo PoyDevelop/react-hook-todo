0cae0313c5da2a19d74262bff150d9fe633e99a4
PoyDev <poydevelop@gmail.com>
2021-08-25 15:00:26 +0700

    add styled component

--- src/App.js
+++ src/App.js
@@ -1,15 +1,39 @@
 import React, { useState } from "react";
 import TodoList from "./TodoList";
 import Say from "./TodoList";
+import styled from "styled-components";
+import PropTypes from "prop-types";
 
 
 
+const MainSection = styled.section`
+background: #a5b7f2;
+padding : 20px;
+
+
+input[type=text] {
+  width: 100%;
+  padding: 12px 20px;
+  margin: 8px 0;
+  box-sizing: border-box;
+}
+`
+const MainBtn = styled.button`
+  width: 100%;
+  height: 35px;
+  padding: 10px;
+  background: #CCC;
+
+  ${props => props.success && `
+  background: #5fde96;
+  border-color: gold;
+  `}
+`
 
 function App() {
 
   const [todoList, setTodoList] = useState([]);
   const [inputText, setInputText] = useState("");
-
 
   // const onTextChange = (e) => setInputText(e.target.value)
   const onTextChange = ({target: {value}}) => {
@@ -37,13 +61,15 @@ function App() {
 
 
   return (
-    <div className="App">
+    <MainSection className="App">
       <input type="text" value ={inputText} placeholder="ป้อนข้อมูล" onChange={onTextChange}/>
-      <button type="button" onClick={addTask}>Save</button>
+      <MainBtn type="button" onClick={addTask}>Save</MainBtn>
+      <MainBtn success onClick={addTask}>Save</MainBtn>
 
       <Say todoList={todoList} removeTask={removeTask}></Say>
 
-    </div>
+
+    </MainSection>
   );
 }
 
